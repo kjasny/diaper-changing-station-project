52ed8ee5ab155afbbfe2bcd653de1fa6e7e87940
kjasny <kalanjasny@gmail.com>
2024-05-01 20:41:23 -0400

    Client side set up

---
+++ index.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const app = express();
+const cors = require('cors')
+const path = require('path')
+
+app.use(cors())
+app.use(express.static("client/build"));
+
+app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+  
+  app.listen(8080, () => {
+    console.log("listening on http://localhost:8080");
+  });
